671a296b484c079ad3a8ec288c110cd89f9819e9
Kenny Ballou <kballou@devnulllabs.io>
2019-12-30 16:54:10 -0700

    services: firewall: use host specific rulesets
    Instead of using the same set of rules for all machines, use specific
    rulesets tailored to each machine.
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- services/firewall.nix
+++ services/firewall.nix
@@ -3,5 +3,5 @@
   # Firewall configuration
   networking.firewall.enable = false;
   networking.nftables.enable = true;
-  networking.nftables.rulesetFile = ./nftables-rules.nft;
+  networking.nftables.rulesetFile = builtins.toPath "/etc/nixos/${config.networking.hostName}/nftables-rules.nft";
 }
