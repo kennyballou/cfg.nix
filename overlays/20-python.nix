59d61fe2c8c9ff1719b9c6c688ecbbf228af769b
Kenny Ballou <kballou@devnulllabs.io>
2020-02-11 10:01:24 -0700

    overlays: python: add pyflakes
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- overlays/20-python.nix
+++ overlays/20-python.nix
@@ -6,6 +6,8 @@ self: super:
       self.python3
       self.python37Packages.gprof2dot
       self.python37Packages.setuptools
+      self.python37Packages.flake8
+      self.python37Packages.pyflakes
       self.python3Packages.pip
       self.python3Packages.virtualenv
     ];
