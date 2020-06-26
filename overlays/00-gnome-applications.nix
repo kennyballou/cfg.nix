1a7a5d17bc194c5a37ffceeec003f1a4464203e2
Kenny Ballou <kballou@devnulllabs.io>
2020-06-26 05:50:41 -0600

    gnome-applications: add a few gnome packages
    These packages are not included in the default `gnome3` set of packages.
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- overlays/00-gnome-applications.nix
+++ overlays/00-gnome-applications.nix
@@ -4,6 +4,8 @@ self: super:
     name = "gnome-applications";
     ignoreCollisions = true;
     paths = [
+      self.gnome3.gnome-autoar
+      self.gnome3.gnome-boxes
       self.gnome3.gnome-tweak-tool
       self.gnome3.gnome-themes-extra
       self.gnome3.gnome-themes-standard
