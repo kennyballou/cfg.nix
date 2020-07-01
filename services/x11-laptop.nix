ba16fb3ed1546e611b92873979d9c5f92bea904f
Kenny Ballou <kballou@devnulllabs.io>
2020-06-30 21:01:27 -0600

    x11-laptop: add libinput settings
    Enable palm detection, set the acceleration profile.  This isn't exactly
    the right settings yet, but committing for now.
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- services/x11-laptop.nix
+++ services/x11-laptop.nix
@@ -3,6 +3,9 @@
   # Enable touchpad support.
   services.xserver.libinput = {
     enable = true;
+    accelProfile = "adaptive";
+    accelSpeed = "0.1";
+    disableWhileTyping = true;
     scrollMethod = "twofinger";
     horizontalScrolling = false;
   };
