64d93814b9f87e82ba216c4c32ecfebbed4cb86e
Kenny Ballou <kb@devnulllabs.io>
2021-04-05 11:15:05 -0600

    containers: add runtimes configuration
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- services/podman.nix
+++ services/podman.nix
@@ -1,10 +1,19 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   virtualisation = {
     podman = {
       enable = true;
       dockerCompat = true;
     };
+    containers.containersConf.extraConfig = ''
+      [engine.runtimes]
+      runc = [
+         "${pkgs.runc}"
+      ]
+      crun = [
+        "${pkgs.crun}"
+      ]
+    '';
     oci-containers.backend = "podman";
   };
 }
