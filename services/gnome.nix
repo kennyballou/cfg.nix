f1163df1851aff6ac558375f2d6da391a9f5c57c
Kenny Ballou <kb@devnulllabs.io>
2021-04-30 12:39:23 -0600

    gnome: exclude some packages
    Remove packages that are never used.
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- services/gnome.nix
+++ services/gnome.nix
@@ -7,6 +7,16 @@
     };
     desktopManager.gnome3.enable = true;
   };
+  environment.gnome3.excludePackages = with pkgs.gnome3; [
+    cheese
+    epiphany
+    gnome-maps
+    gnome-music
+    gnome-photos
+    geary
+    gedit
+    gnome-software
+  ];
   programs.dconf.enable = true;
   services.dbus.packages = with pkgs; [ gnome2.GConf ];
   services.udev.packages = with pkgs; [
