c2a39595d9a9d6b18bbea3cf380292264e8a96a5
Kenny Ballou <kb@devnulllabs.io>
2021-05-12 12:50:20 -0600

    gnome: update gnome keys
    New release has renamed various keys.
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- services/gnome.nix
+++ services/gnome.nix
@@ -5,17 +5,18 @@
     displayManager.gdm = {
       enable = true;
     };
-    desktopManager.gnome3.enable = true;
+    desktopManager.gnome.enable = true;
   };
-  environment.gnome3.excludePackages = with pkgs.gnome3; [
-    cheese
-    epiphany
-    gnome-maps
-    gnome-music
-    gnome-photos
-    geary
-    gedit
-    gnome-software
+  environment.gnome.excludePackages = with pkgs; [
+    gnome3.cheese
+    gnome3.epiphany
+    gnome3.gnome-maps
+    gnome3.gnome-music
+    gnome3.gnome-photos
+    gnome3.geary
+    gnome3.gedit
+    gnome3.gnome-software
+    gnome-tour
   ];
   programs.dconf.enable = true;
   services.dbus.packages = with pkgs; [ gnome2.GConf ];
