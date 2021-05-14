45945464319e81a70dbed35ca20f26c1643a7079
Kenny Ballou <kb@devnulllabs.io>
2021-05-14 10:43:42 -0600

    networkmanager: append some sane nameservers
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- services/networkmanager.nix
+++ services/networkmanager.nix
@@ -4,5 +4,12 @@
     enable = true;
     # dnsmasq will handle this...
     dns = "none";
+    appendNameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "9.9.9.9"
+      "208.67.222.222"
+      "208.67.220.220"
+    ];
   };
 }
