73bfe7ec9eaf1aa1943f6f2284fbc8fc22819f58
Kenny Ballou <kb@devnulllabs.io>
2021-05-14 10:43:12 -0600

    overlays: kde: update package list
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- overlays/00-kde-applications.nix
+++ overlays/00-kde-applications.nix
@@ -3,45 +3,47 @@ self: super:
   kde-applications = self.buildEnv {
     name = "kde-applications";
     ignoreCollisions = true;
-    paths = with self; [
+    paths = with self.libsForQt5; [
       kde-gtk-config
       kdeplasma-addons
-      kdeApplications.akonadi
-      kdeApplications.akonadi-calendar
-      kdeApplications.akonadi-contacts
-      kdeApplications.akonadi-mime
-      kdeApplications.akonadi-notes
-      kdeApplications.akonadi-search
-      kdeApplications.akonadiconsole
-      kdeApplications.ark
-      kdeApplications.dolphin
-      kdeApplications.dolphin-plugins
-      kdeApplications.gwenview
-      kdeApplications.kaddressbook
-      kdeApplications.kalarm
-      kdeApplications.kalarmcal
-      kdeApplications.kcachegrind
-      kdeFrameworks.kcontacts
-      kdeApplications.kcharselect
-      kdeApplications.kdav
-      kdeApplications.kdenetwork-filesharing
-      kdeApplications.kdepim-apps-libs
-      kdeApplications.kdepim-runtime
-      kdeApplications.kidentitymanagement
-      kdeApplications.kimap
-      kdeApplications.kio-extras
-      kdeApplications.kmbox
-      kdeApplications.kmime
-      kdeApplications.kmplot
-      kdeApplications.korganizer
-      kdeApplications.kompare
-      kdeApplications.libkdepim
-      kdeApplications.okular
-      kdeApplications.pimcommon
-      kdeApplications.ksmtp
-      kdeApplications.spectacle
-      plasma5.plasma-browser-integration
-      plasma5.user-manager
+      akonadi
+      akonadi-calendar
+      akonadi-contacts
+      akonadi-mime
+      akonadi-notes
+      akonadi-search
+      akonadiconsole
+      ark
+      dolphin
+      dolphin-plugins
+      gwenview
+      kaccounts-integration
+      #kaccounts-providers
+      kaddressbook
+      kalarm
+      kalarmcal
+      kcachegrind
+      kcontacts
+      kcharselect
+      kdav
+      kdeconnect-kde
+      kdenetwork-filesharing
+      kdepim-runtime
+      kidentitymanagement
+      kimap
+      kio-extras
+      kmbox
+      kmix
+      kmime
+      kmplot
+      korganizer
+      kompare
+      libkdepim
+      okular
+      pimcommon
+      ksmtp
+      spectacle
+      yakuake
     ];
   };
 }
